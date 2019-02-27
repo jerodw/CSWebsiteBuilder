b8ea517bb779a403d596ad47e4ed9cbe31ef24b9
RyanRemer <ryandremer@gmail.com>
2019-02-26 18:06:57 -0700

    Fixed not getting correct date

--- CSWebsiteBuilder/src/app/models/Resource.ts
+++ CSWebsiteBuilder/src/app/models/Resource.ts
@@ -1,11 +1,13 @@
 export class Resource{
+    static DEFAULT_DATE = new Date();
     title: string;
     downloadLink: string;
     avaliableDate: Date;
 
-    constructor(title:string, downloadLink:string, avaliableDate = new Date()){
+    constructor(title:string, downloadLink:string, avaliableDate = Resource.DEFAULT_DATE){
         this.title = title;
         this.downloadLink = downloadLink;
+        // the date that this resource will be viewable
         this.avaliableDate = avaliableDate;
     }
 }
