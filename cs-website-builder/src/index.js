1182d3a7dc2bf00e5d7082efa07958e4acc01822
Sotaur <christianmarklambert@gmail.com>
2019-03-01 18:04:26 -0700

    Updated package.json

--- cs-website-builder/src/index.js
+++ cs-website-builder/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import fs from 'fs';
+import * as serviceWorker from './serviceWorker';
 
-const index = ReactDOMServer.renderToString( < App / > , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-fs.writeFile(index, 'index.html');
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
