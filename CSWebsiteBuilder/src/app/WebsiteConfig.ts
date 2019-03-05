912d0d92b368c135b088a789a2d99a55c89b187d
RyanRemer <ryandremer@gmail.com>
2019-03-05 16:53:36 -0700

    Policies

--- CSWebsiteBuilder/src/app/WebsiteConfig.ts
+++ CSWebsiteBuilder/src/app/WebsiteConfig.ts
@@ -7,6 +7,8 @@ import { WeekDay } from "@angular/common";
 import { TAOffice } from "./models/TAOffice";
 import { Professor } from "./models/Professor";
 import { Announcement } from "./models/Announcement";
+import { Policy } from "./models/Policy";
+import { GradeCutOff } from "./models/GradingCutOff";
 
 export class WebsiteConfig {
     courseName: string;
@@ -17,6 +19,8 @@ export class WebsiteConfig {
     taOffice: TAOffice;
     courseTAs: TA[];
     announcements: Announcement[];
+    policies: Policy[];
+    gradingScale: GradeCutOff[];
 
     constructor() {
         // name of the course you are teaching
@@ -55,6 +59,7 @@ export class WebsiteConfig {
         this.navigationLinks = [
             new NavigationLink("Schedule", "/schedule"),
             new NavigationLink("TA Information", "/ta-information"),
+            new NavigationLink("Policies", "/policies"),
         ];
 
         // the relaive file path to resources
@@ -93,6 +98,44 @@ export class WebsiteConfig {
                 "Welcome to class!",
                 "<p>We hope you have a great semester. </p>"
             )
+        ]
+
+        //list the grade cut offs
+        this.gradingScale = [
+            new GradeCutOff("A", 93, 100),
+            new GradeCutOff("A-", 90, 93),
+            new GradeCutOff("B+", 87, 89),
+            new GradeCutOff("B", 83, 86),
+            new GradeCutOff("B-", 80, 82),
+            new GradeCutOff("C+", 77, 79),
+            new GradeCutOff("C", 73, 76),
+            new GradeCutOff("C-", 70, 72),
+            new GradeCutOff("D+", 67, 69),
+            new GradeCutOff("D", 63, 66),
+            new GradeCutOff("D-", 60, 62),
+            new GradeCutOff("E", 0, 60),
+        ]
+
+        // add the polices that you have for your class
+        this.policies = [
+            new Policy(
+                "Technology in Class",
+                "We have learned by sad experience that technology is a major distraction to students in class, and prevents them from listening, participating, and learning as they should. Therefore, you may not use laptops, tablets, phones, or other technology during lecture. Notes may be taken using good old-fashioned paper and pen(cil). However, when you are working with your group or doing other non-lecture activities, you may use	your technology as needed."
+            ),
+            new Policy(
+                "Honor Code Standards",
+                "In keeping with the principles of the BYU Honor Code, students are expected to be honest in all of their academic work. Academic honesty means, most fundamentally, that any work you present as your own must in fact be your own work and not that of another. Violations of this principle may result in a failing grade in the course and additional disciplinary action by the university." +
+                "<br>" +
+                "Students are also expected to adhere to the Dress and Grooming Standards. Adherence demonstrates respect for yourself and others and ensures an effective learning and working environment. It is the university's expectation, and my own expectation in class, that each student will abide by all Honor Code standards. Please call the Honor Code Office at 422-2847 if you have questions about those standards."
+            ),
+            new Policy(
+                "Harassment",
+                "Harassment of any kind is inappropriate at BYU. Specifically, BYU's policy against sexual harassment extends not only to employees of the university but to students as well. If you encounter sexual harassment, gender-based discrimination, or other inappropriate behavior, please talk to your professor, contact the Equal Employment Office at 422-5895 or 367-5689, or contact the Honor Code Office at 422-2847."
+            ),
+            new Policy(
+                "Disabilities",
+                "BYU is committed to providing reasonable accommodation to qualified persons with disabilities. If you have any disability that may adversely affect your success in this course, please contact the University Accessibility Center at 422-2767. Services deemed appropriate will be coordinated with the student and instructor by that office."
+            ),
         ]
     }
 }
--- CSWebsiteBuilder/src/app/app.module.ts
+++ CSWebsiteBuilder/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { NavigationBarComponent } from './widgets/navigation-bar/navigation-bar.
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
 import { ProfessorViewComponent } from './widgets/professor-view/professor-view.component';
+import { PolicyPageComponent } from './pages/policy-page/policy-page.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'ta-information', component: TaInfoPageComponent },
   { path: 'schedule', component: ScheduleComponent },
+  { path: 'policies', component: PolicyPageComponent },
   {
     path: '',
     redirectTo: '/home',
@@ -30,7 +32,8 @@ const appRoutes: Routes = [
     NavigationBarComponent,
     PageNotFoundComponent,
     ScheduleComponent,
-    ProfessorViewComponent
+    ProfessorViewComponent,
+    PolicyPageComponent
   ],
   imports: [
     RouterModule.forRoot(
