5d83fb672f73b34dd37f1b0a6faa71d9380bf00b
RyanRemer <ryandremer@gmail.com>
2019-03-13 14:38:01 -0600

    made it so we build to a folder

--- cs-website-builder/src/index.jsx
+++ cs-website-builder/src/index.jsx
@@ -3,8 +3,19 @@ import ReactDOMServer from 'react-dom/server';
 import {App} from './App.jsx';
 import fs from 'fs';
 
-const template = fs.readFileSync('./templates/template-test.html', { 'encoding': 'utf8' });
+// builds the build directory if it doesn't exist
+const buildDirectory = "./build/"
+if (!fs.existsSync(buildDirectory)){
+    fs.mkdirSync(buildDirectory);
+}
 
+const template = fs.readFileSync('./templates/template-test.html', { 'encoding': 'utf8' });
 const index = ReactDOMServer.renderToString( < App template={ template } / > );
 
-fs.writeFile('index.html', index, (err) => err);
+fs.writeFile(buildDirectory + 'index.html', index, function (err){
+    if (err){
+        console.error(err);
+        return;
+    }
+    console.log("built website to " + __dirname + "\\build\\");
+});
